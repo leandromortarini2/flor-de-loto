9da25ab67417b4ab31b223adf1f135a7a9afed6a
Agustina Sosa <agustinaayelensosa2@gmail.com>
2025-02-27 16:23:49 -0300

    fix hero background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,43 @@
-"use client";
 import { About } from "./components/About/About";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+import Image from "next/image";
+import rectangleHero from "../../public/assets/rectangleHero.png";
 import { Footer } from "./components/Footer/Footer";
 import { Products } from "./components/Products/Products";
 import { WhatsappButton } from "./components/WhatsappButton/WhatsappButton";
-import { useRotate } from "./hooks/useRotate";
-import Image from "next/image";
 export default function Home() {
-  const rotateScreen = useRotate();
-
   return (
-    <div
-      className={`w-full bg-bgHeroMovile lg:bg-bgHero  bg-cover    relative  ${
-        rotateScreen ? "h-auto" : "h-screen"
-      }`}
-    >
-      <div className="absolute inset-0 z-0 h-full bg-black/50"></div>
-      <div className="absolute inset-0 bottom-0 z-10 items-end justify-end hidden w-full lg:flex h-1/3">
-        <Image
-          src={"/assets/rectangleHero.png"}
-          alt="hero-rectangle"
-          width={500}
-          height={200}
-          className="w-full h-2/3"
-        />
-      </div>
-      <div id="hero" className="relative z-10 w-full min-h-screen text-center ">
-        <Navbar />
-        <div
-          className={`w-full  md:h-auto  flex justify-center  items-start xl:items-center lg:overflow-y-hidden  ${
-            rotateScreen ? "h-auto" : "h-screen"
-          }`}
-        >
-          <Hero />
-        </div>
+    <>
+      {/* caja para darle la opacidad al fondo en movile */}
+      <div className="absolute inset-0 bg-black/50 lg:hidden"></div>
+      {/* w-full  h-screen bg-bgHeroMovile lg:bg-portada  bg-cover */}
+      <div
+        className={`w-full  h-screen  bg-bgHeroMovile lg:bg-bgHero  bg-cover`}
+      >
+        <div className="hidden lg:block w-full p-0 xs:min-h-[700px] sm:min-h-[630px] md:min-h-screen lg:bg-black/80 absolute inset-0 z-0"></div>
 
+        <div
+          id="hero"
+          className="w-full min-h-screen relative z-10 text-center  "
+        >
+          <Navbar />
+          <div className="w-full h-screen  md:h-auto  flex justify-center items-start xl:items-center lg:overflow-y-hidden relative  ">
+            <Hero />
+            <div className="hidden lg:block w-full z-10 bottom-0 lg:bottom-20 absolute ">
+              <Image
+                src={rectangleHero}
+                alt="hero"
+                className="w-full h-full lg:h-auto object-cover  "
+              />
+            </div>
+          </div>
+        </div>
         <About />
         <Products />
         <WhatsappButton />
         <Footer />
       </div>
-    </div>
+    </>
   );
 }
